54aaebf8fcdf03931e277d3379039a2e8e925669
Dachi <151663998+dachico@users.noreply.github.com>
2024-02-21 00:30:41 +0200

    Add files via upload

---
+++ _challenge--start/js/index.js
@@ -0,0 +1,68 @@
+function getCountryPopulation(country){
+    return new Promise((resolve,reject)=> {
+        const url = `https://countriesnow.space/api/v0.1/countries/population`;
+        const options = {
+          method: 'POST', 
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({country})
+        };
+        fetch(url,options)
+            .then(res => res.json())
+            .then(json => {
+                if (json?.data?.populationCounts) resolve(json.data.populationCounts.at(-1).value);
+                else reject(new Error(`My Error: no data for ${country}`)) //app logic error message
+            })
+            .catch(err => reject(err)) // network error - server is down for example...
+            // .catch(reject)  // same same, only shorter... 
+    })
+}
+
+
+//--------------------------------------------------------
+//  Manual - call one by one...
+//--------------------------------------------------------
+function manual() {
+    getCountryPopulation("France")
+        .then( population => {
+            console.log(`population of France is ${population}`);
+            return getCountryPopulation("Germany")
+        })
+        .then( population => console.log(`population of Germany is ${population}`))
+        .catch(err=> console.log('Error in manual: ',err.message));
+}
+// manual()
+
+
+//------------------------------
+//   Sequential processing 
+//------------------------------
+const countries = ["France","Russia","Germany","United Kingdom","Portugal","Spain","Netherlands","Sweden","Greece","Czechia","Romania","Israel"];
+
+function sequence() {
+    Promise.each(countries, country => {
+        return getCountryPopulation(country)
+        .then(population => console.log(`population of ${country} is ${population}`))
+        .catch(err => console.error(`Error: no data found for ${country}`))
+    })
+    .then(() => console.log('all done! 🏋️'))
+    
+
+}
+// sequence();
+
+//--------------------------------------------------------
+//  Parallel processing 
+//--------------------------------------------------------
+function parallel() {
+    Promise.map(countries, (country, index) => {
+        return Promise.delay(index * 2000)
+        .then(() => getCountryPopulation(country))
+        .then(population => console.log(`population of ${country} is ${population}`))
+        .catch(err => console.error(`Error: no data found for ${country}`))
+    })
+    .then(() => console.log('all done! 🏋️'))
+
+}
+parallel();
